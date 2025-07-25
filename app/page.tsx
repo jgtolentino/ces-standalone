77185f0d192cc90ebe0745cabd939567dc5de5d1
jgtolentino <jgtolentino_rn@yahoo.com>
2025-07-26 03:27:50 +0800

    fix: show full Scout dashboard on homepage instead of basic Overview

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import Overview from "../components/Overview";
+'use client';
+
+import ScoutDashboardV3 from '../components/scout/scout-dashboard-v3';
 
 export default function Page() {
-  return <Overview />;
+  return <ScoutDashboardV3 />;
 }
